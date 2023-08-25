de695d41b75f6e80460a1158869fb67bf1ca6c05
Supakorn.Film <nongfilmcry@gmail.com>
2023-08-25 17:20:29 +0700

    Add public folder
    add public & html file

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "/public/"))); // ? เป็นการบอกว่า Path ของ HTML, CSS, JS เราอยู่ที่โฟลเดอร์อะไร
 
 // ? เอาไว้จัดการกับ Request ที่เข้ามาตรง ๆ ผ่านหน้านี้จะส่งข้อมูลอะไรกลับไป
 app.get("/", (req, res) => {
