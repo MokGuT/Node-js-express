1dd214a8199740210a6dabd6b7ae446e1f354ca1
Supakorn.Film <nongfilmcry@gmail.com>
2023-08-27 17:12:27 +0700

    add ejs module

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, "/public/"))); // ? เป็นการบอกว่า Path ของ HTML, CSS, JS เราอยู่ที่โฟลเดอร์อะไร
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
 // ? เอาไว้จัดการกับ Request ที่เข้ามาตรง ๆ ผ่านหน้านี้จะส่งข้อมูลอะไรกลับไป
 app.get("/", (req, res) => {
-   res.send('Hello born to dev');
+   // res.send('Hello born to dev'); // ? ถ้าจะใช้ EJS เราจะไม่ใช้ .send แต่จะใช้ .render แทน
+   res.render('index', {username: 'Supakorn', customer: ["Film", "Dongkew", "EIEI"]}); // ? Parameter ตัวที่สองคือข้อมูลที่เราต้องการส่งเข้าไปให้หน้าเว็บของเรา
 });
 
 app.listen(PORT, () => {
